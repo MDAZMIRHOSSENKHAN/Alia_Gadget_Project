0b1276a3d9e334d5fae60894ad8bafef458a7f7f
RafiulAlam98 <mrafiul.alam7@gmail.com>
2024-03-14 23:13:24 +0600

    updated latest code

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import HomePage from "../pages/homepage/HomePage";
 
 const MainRoutes = createBrowserRouter([
     {
         path:"/",
-        element:<MainLayout/>
+        element:<MainLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<HomePage/>
+            },
+            {
+                path:"/homepage",
+                element:<HomePage/>
+            },
+        ]
     }
 ])
 
---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import Carousel from "../../carousel/Carousel";
+
+const HomePage = () => {
+    return (
+        <>
+         <Carousel/>   
+        </>
+    );
+};
+
+export default HomePage;
