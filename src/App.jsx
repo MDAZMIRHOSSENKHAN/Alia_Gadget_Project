5300cd8b3e96a4fd97de6f2dfb875c847bfdc1ce
RafiulAlam98 <mrafiul.alam7@gmail.com>
2024-03-14 16:54:48 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import MainRoutes from './Routes/MainRoutes'
 
 function App() {
 
   return (
     <>
-     
+     <RouterProvider router={MainRoutes}/>
     </>
   )
 }
