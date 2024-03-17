385ea34b38e647c74a53a83445aa8deaa042cbfe
RafiulAlam98 <mrafiul.alam7@gmail.com>
2024-03-17 17:40:35 +0600

    updated

--- src/pages/login/SignUp.jsx
+++ src/pages/login/SignUp.jsx
@@ -1,9 +1,35 @@
+import { useForm } from "react-hook-form";
+
 const SignUp = () => {
-    return (
-        <div>
-            Sign Up page coming soon...
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="m-auto w-1/2">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+        <input
+        className="input input-bordered input-info w-full max-w-xs "
+        defaultValue=""
+        {...register("name")}
+      />
         </div>
-    );
+
+        <input
+          className="input input-bordered input-info w-full max-w-xs"
+          {...register("email", { required: true })}
+        />
+        {errors.email && <span>This field is required</span>}
+
+        <input className="btn btn-primary" type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
